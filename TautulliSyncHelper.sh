c9896e841674c6ce0f2bca7d52839e74322e09d4
Bas van den Wollenberg <hi@bas.sh>
2024-03-31 20:33:52 +0200

    Use Python install in Docker container instead of venv

--- TautulliSyncHelper.sh
+++ TautulliSyncHelper.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [[ ! -d venv ]]; then
-	python3 -m venv venv
-fi
-./venv/bin/pip install -U pip
-./venv/bin/pip install -r requirements.txt
+pip install -U pip
+pip install -r requirements.txt
 
-/home/bas/git/PlexAniSync/venv/bin/python3 ./TautulliSyncHelper.py "$1"
+if [ $# -eq 0 ]; then
+	python ./PlexAniSync.py
+else
+	python ./TautulliSyncHelper.py "$1"
+fi
