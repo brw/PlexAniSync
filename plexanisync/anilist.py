ebd996e430be03c322f045c62b61fb8cb1082353
Bas van den Wollenberg <hi@bas.sh>
2024-05-26 19:51:02 +0200

    Remove completed rewatch check

--- plexanisync/anilist.py
+++ plexanisync/anilist.py
@@ -566,13 +566,6 @@ class Anilist:
                 and anilist_media_status == "FINISHED"
             ):
                 # series completed watched
-                if status == "REPEATING":
-                    logger.warning(
-                        f"Plex episode watch count [{watched_episode_count}] was higher than the one "
-                        f"on AniList [{anilist_episodes_watched}] which has total of {anilist_total_episodes} "
-                        f"episodes | However, series is marked as REPEATING so skipping update"
-                    )
-                    return
                 logger.warning(
                     f"Plex episode watch count [{watched_episode_count}] was higher than the "
                     f"one on AniList total episodes for that series [{anilist_total_episodes}] | updating "
